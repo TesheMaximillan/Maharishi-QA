31ae3130d8f9eec2f8b9344fae7e34dc83a27734
Teshome Kurabachew <dear.teshe@gmail.com>
2022-04-23 23:45:02 +0300

    Refactor for redability

--- src/main/java/com/example/Madhav.java
+++ src/main/java/com/example/Madhav.java
@@ -9,17 +9,18 @@ public class Madhav {
    */
   public int isMadhavArray(int[] a) {
     int isMadhav = 0, base = a[0], partialSum, n = 2;
-    
+
     for (int i = 1; i < a.length; ) {
       partialSum = 0;
       if (n * (n + 1)/2 > a.length) return isMadhav;
       for (int j = i; j < n+i; j++) {
         partialSum += a[j];
       }
+      if (base != partialSum) return isMadhav;
       i += n;
       n++;
-      if (base != partialSum) return isMadhav;
     }
+    
     isMadhav = 1;
     return isMadhav;
   }
